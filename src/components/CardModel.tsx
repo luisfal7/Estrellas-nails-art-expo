e0ea6180f91670f695e7e060e41e37136910dd0c
Luis Falconi <luism.falconi@gmail.com>
2023-01-28 22:33:16 -0300

    modif ModelsScreen

--- src/components/CardModel.tsx
+++ src/components/CardModel.tsx
@@ -8,37 +8,42 @@ interface Props {
 }
 
 export const CardModel = ({ model }: Props) => {
+
   return (
-    <View>
-      <Card
-        containerStyle={styles.containerCard}
-      >
-        <Card.Image
-          style={{ resizeMode: 'contain', borderTopLeftRadius:10, borderTopRightRadius:10 }}
-          source={{
-            uri: model.model,
-          }}
-        />
-        <View style={styles.containerBtn}>
-            <Button radius={"md"} type="clear">
-                <Icon name="trash-outline" type='ionicon' color="gray" />
-            </Button>
-        </View>
-      </Card>
+    <View style={ styles.container }>
+      <Card.Image
+        style={styles.image}
+        source={{
+          uri: model.model,
+        }}
+      />
+      <View style={styles.containerBtn}>
+        <Button radius={"md"} type="clear">
+          <Icon name="trash-outline" type="ionicon" color="gray" />
+        </Button>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    containerCard:{
-        borderRadius: 10,
-        width: 200,
-        padding:0,
-        //margin:0
-    },
-    containerBtn:{
-        flexDirection: 'row',
-        marginTop: 10,
-        justifyContent: 'flex-end'
-    }
+  container: {
+    flexDirection:'row',
+    borderWidth:1,
+    borderColor:'gray',
+    borderRadius:10,
+    margin:5,
+  },
+  image:{
+    resizeMode: "stretch",
+    borderRadius:10,
+    width: 200,
+    height: 200,
+    paddingVertical: 5
+  },
+  containerBtn: {
+    marginVertical: 5,
+    marginHorizontal:5,
+    justifyContent: "flex-end",
+  },
 });
--- src/screens/ModelsScreen.tsx
+++ src/screens/ModelsScreen.tsx
@@ -1,20 +1,44 @@
 import React from 'react'
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, ActivityIndicator, StyleSheet } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { CardModel } from '../components/CardModel';
 import { useEstrellas } from '../hooks/useEstrellas';
 
 export const ModelsScreen = () => {
 
-  const {isLoading, clients, models} = useEstrellas()
+  const {isLoading, models} = useEstrellas()
+  const{ top } = useSafeAreaInsets()
 
   return (
-    <View>
-      <FlatList 
-        data={ models }
-        renderItem= {({item}) => (
-          <CardModel model={ item }/>
-          )}
-      />
+    <View style={{...styles.container, top: top + 10 }}>
+      <Text style={ styles.title }>Lista de Modelos</Text>
+      {
+        isLoading ? 
+        <ActivityIndicator size="large" />
+        : 
+        (
+          <FlatList 
+            data={ models }
+            renderItem= {({item}) => (
+              <CardModel model={ item }/>
+              )}
+              style={ styles.ListModels }
+          />
+        )
+      }
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    
+  },
+  title: {
+    alignSelf: 'center',
+    padding: 20
+  },
+  ListModels: {
+    alignSelf: 'center'
+  }
+})
