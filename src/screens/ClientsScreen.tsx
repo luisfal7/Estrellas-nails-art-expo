e1669c5581df9c81a0a8f97f5aba482a54498c3d
Luis Falconi <luism.falconi@gmail.com>
2023-02-02 14:00:42 -0300

    modif ClientsScreen

--- src/screens/ClientsScreen.tsx
+++ src/screens/ClientsScreen.tsx
@@ -1,25 +1,46 @@
-import React, { useContext, useEffect } from 'react'
-import { View, Text, FlatList } from 'react-native';
-import { CardClient } from '../components/CardClient';
-import { ApiEstrellaContext } from '../context/ApiEstrellaContext';
+import React, { useContext, useEffect } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+} from "react-native";
+import { useTheme } from "@react-navigation/native";
+import { ApiEstrellaContext } from "../context/ApiEstrellaContext";
+import { CardClient } from "../components/CardClient";
 
 export const ClientsScreen = () => {
 
-  const {clients, getClients} = useContext(ApiEstrellaContext)
+  const { colors } = useTheme();
+  const { clients, getClients, isLoading } = useContext(ApiEstrellaContext);
 
-  useEffect(()=>{
-    getClients()
-  },[])
+  useEffect(() => {
+    getClients();
+  }, []);
 
   return (
-    <View style={{ flex:1 }}>
-      <FlatList 
-        data={ clients }
-        keyExtractor={((item) => item.id)}
-        renderItem= {({item}) => (
-          <CardClient client={ item }/>
-          )}
-      />
-  </View>
-  )
-}
+    <View style={{ flex: 1 }}>
+      {isLoading ? (
+        <ActivityIndicator color={colors.primary} size={"large"} />
+      ) : (
+        <View>
+          <Text style={styles.title}>Lista de Clientes</Text>
+          <FlatList
+            data={clients}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <CardClient client={item} />}
+          />
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+  title: {
+    alignSelf: "center",
+    padding: 20,
+  },
+});
