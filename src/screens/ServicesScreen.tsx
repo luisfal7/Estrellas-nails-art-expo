04f996b1dbaf874cbf2e0820408f12ccae0b818a
Luis Falconi <luism.falconi@gmail.com>
2023-02-07 10:20:27 -0300

    modif flatlist services

--- src/screens/ServicesScreen.tsx
+++ src/screens/ServicesScreen.tsx
@@ -1,68 +1,84 @@
-import React, { useContext, useEffect } from 'react'
-import { View,Text, StyleSheet, FlatList, Dimensions, ActivityIndicator } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { CardService } from '../components/CardService';
-import { ApiEstrellaContext } from '../context/ApiEstrellaContext';
+import React, { useContext, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { CardService } from "../components/CardService";
+import { ApiEstrellaContext } from "../context/ApiEstrellaContext";
 import { useTheme } from "@react-navigation/native";
 
 export const ServicesScreen = () => {
-
   const { top } = useSafeAreaInsets();
 
   const { colors } = useTheme();
 
-  const { getServices, services, isLoading } = useContext(ApiEstrellaContext)
+  const { getServices, services, isLoading } = useContext(ApiEstrellaContext);
 
-  const dimensionWidth = Dimensions.get('window').width
-  const dimensionHeight = Dimensions.get('window').height
+  const dimensionWidth = Dimensions.get("window").width;
+  const dimensionHeight = Dimensions.get("window").height;
 
-  useEffect(()=>{
-    getServices()
-  },[])
+  useEffect(() => {
+    getServices();
+  }, []);
 
   return (
-    <View style={{ ...styles.container, top: top + 10 }}>
-      {
-        isLoading ? 
-        (<ActivityIndicator
-        color={colors.primary}
-        size={"large"}
-        style={{
-          ...styles.indicator,
-          top: dimensionHeight * 0.5,
-          right: dimensionWidth * 0.5,
-        }}
-        />) :
-        <View> 
-          <Text style={styles.title}>
-              Lista de servicios
-          </Text>
-  
+    <View style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator
+          color={colors.primary}
+          size={"large"}
+          style={{
+            ...styles.indicator,
+            top: dimensionHeight * 0.5,
+            right: dimensionWidth * 0.5,
+          }}
+        />
+      ) : (
+        <View>
           <FlatList
             data={services}
             renderItem={({ item, index }) => (
               <CardService service={item} index={index} />
-              )}
-            style={styles.listServices}
+            )}
+            ListHeaderComponent={
+              <View
+                style={{
+                  ...styles.containerListHeader,
+                  backgroundColor: colors.background,
+                  borderBottomColor: colors.primary,
+                  top: top,
+                }}
+              >
+                <Text style={styles.title}>Lista de Servicios</Text>
+              </View>
+            }
+            stickyHeaderIndices={[0]}
           />
         </View>
-      }
+      )}
     </View>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
   container: {},
-  title: {
-    alignSelf: "center",
-    padding: 20,
+  containerListHeader: {
+    height: 50,
+    width: "100%",
+    paddingTop: 10,
+    borderBottomWidth: 1,
   },
-  listServices:{
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
     alignSelf: "center",
-    marginBottom: 100
   },
   indicator: {
     position: "absolute",
   },
-
 });
